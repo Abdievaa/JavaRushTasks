1bd93298136fe5919fd1227914bc7820272820dc
Aida <abdieva18@mail.ru>
2022-09-29 19:37:39 +0600

    add solution JavaSyntax from task 0107

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 3;
+        int numericValue = 5;
+        int size = 9;
+        int number = 23;
+
+    }
+}
