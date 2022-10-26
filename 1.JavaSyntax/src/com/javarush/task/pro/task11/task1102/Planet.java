6fe3bde87e4b391ea574b03998f96e0a96433907
Aida <abdieva18@mail.ru>
2022-10-26 17:22:05 +0600

    add solution JavaSyntax from task 1102

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1102/Planet.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1102/Planet.java
@@ -7,11 +7,8 @@ package com.javarush.task.pro.task11.task1102;
 public class Planet {
 
     public String name;
-
     public  long age;
-
     public int speed;
-
     public  int area;
 
     public void printInformation() {
