be0014dc8edc8b9f7e1946d18f7ea8454549dca0
Aida <abdieva18@mail.ru>
2022-11-06 20:56:20 +0600

    add solution for game 2048 step 1 of 18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -3,5 +3,13 @@ package com.javarush.games.game2048;
 import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
+    private static final int SIDE = 4;
 
+
+
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE,SIDE);
+    }
 }
+//--module-path "/Users/aliiaabdyeva/Documents/Mega/JavaRushTasks/lib/javafx-sdk-17.0.2/lib" --add-modules javafx.controls,javafx.fxml
