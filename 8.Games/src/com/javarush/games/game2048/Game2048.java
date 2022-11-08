ed746bd21400af6d76a29328e6a41368de4bb06b
Aida <abdieva18@mail.ru>
2022-11-08 22:55:12 +0600

    add solution for game 2048 step 1 of 18

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -4,12 +4,33 @@ import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
     private static final int SIDE = 4;
+    private int [][] gameField = new int[SIDE][SIDE];
+
+    private void createGame(){}
+
+    private void drawScene(){
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                setCellColor(j,i, Color.BLUE);
+
+            }
+
+
+        }
+    }
+    private void createNewNumber(){
+        getRandomNumber(SIDE);
+        getRandomNumber(SIDE);
+
+    }
 
 
 
     @Override
     public void initialize() {
         setScreenSize(SIDE,SIDE);
+        createGame();
+        drawScene();
     }
 }
 //--module-path "/Users/aliiaabdyeva/Documents/Mega/JavaRushTasks/lib/javafx-sdk-17.0.2/lib" --add-modules javafx.controls,javafx.fxml
