ed9d2ec048beee7e66970385739baad73bbb554e
Aida <abdieva18@mail.ru>
2022-11-17 22:06:47 +0600

    add solution task13

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0907/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.pro.task09.task0907;
+
+import java.util.regex.Pattern;
+
+/* 
+Шестнадцатеричный конвертер
+*/
+
+public class Solution {
+    private static final String HEX = "0123456789abcdef";
+
+    public static void main(String[] args) {
+        int decimalNumber = 1256;
+        System.out.println("Десятичное число " + decimalNumber + " равно шестнадцатеричному числу " + toHex(decimalNumber));
+        String hexNumber = "4e8";
+        System.out.println("Шестнадцатеричное число " + hexNumber + " равно десятичному числу " + toDecimal(hexNumber));
+    }
+
+    public static String toHex(int decimalNumber) {
+        //напишите тут ваш код
+        return null;
+    }
+
+    public static int toDecimal(String hexNumber) {
+        //напишите тут ваш код
+        return 0;
+    }
+}
