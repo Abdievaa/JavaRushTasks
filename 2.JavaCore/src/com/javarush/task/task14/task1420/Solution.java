25aaea2b203e0d09e71794b5285f726a76624b1e
Aida <abdieva18@mail.ru>
2022-12-13 22:41:58 +0600

    add solution Java Core task 1420

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task14.task1420;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+НОД
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+
+    }
+}
