e6eccfbf874f716adfe5c7cc8bcf2dc308ec08cc
Aida <abdieva18@mail.ru>
2023-01-24 22:12:56 +0600

    add solution Java Syntax task 1318

--- 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task13.task1318;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
@@ -10,7 +11,14 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        // напишите тут ваш код
+    public static void main(String[] args) throws IOException {
+        try (Scanner scanner = new Scanner(System.in);
+             FileInputStream fileInputStream = new FileInputStream(scanner.nextLine());
+             BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                System.out.println(line);
+            }
+        }
     }
 }
