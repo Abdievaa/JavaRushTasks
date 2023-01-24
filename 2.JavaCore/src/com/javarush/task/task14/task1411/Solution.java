9adb9c803a32979e5fb8b52a03d0fd8dc66a881e
Aida <abdieva18@mail.ru>
2023-01-24 22:15:23 +0600

    add solution Java Syntax task 1411

--- 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -10,19 +10,36 @@ User, Loser, Coder and Proger
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Person person = null;
-        String key = null;
-
-        //тут цикл по чтению ключей, пункт 1
-        {
-            //создаем объект, пункт 2
-
-            doWork(person); //вызываем doWork
-
+        while (true) {
+           String key = reader.readLine();
+            if (key.equalsIgnoreCase("user")) {
+                doWork(new Person.User());
+            } else if (key.equalsIgnoreCase("loser")) {
+                doWork(new Person.Loser());
+            } else if (key.equalsIgnoreCase("coder")) {
+                doWork(new Person.Coder());
+            } else if (key.equalsIgnoreCase("proger")) {
+                doWork(new Person.Proger());
+            } else {
+                break;
+            }
         }
     }
 
     public static void doWork(Person person) {
-        // пункт 3
+        if (person instanceof Person.User) {
+            Person.User user = (Person.User) person;
+            user.live();
+        } else if (person instanceof Person.Loser) {
+            Person.Loser loser = (Person.Loser) person;
+            loser.doNothing();
+        } else if (person instanceof Person.Coder) {
+            Person.Coder coder = (Person.Coder) person;
+            coder.writeCode();
+
+        } else if (person instanceof Person.Proger) {
+            Person.Proger proger = (Person.Proger) person;
+            proger.enjoy();
+        }
     }
 }
