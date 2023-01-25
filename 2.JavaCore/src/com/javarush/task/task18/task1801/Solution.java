6c33ac780d068cacd5d0f533aa0658f65cd623a4
Aida <abdieva18@mail.ru>
2023-01-25 22:17:04 +0600

    add solution Java Syntax task 1801

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task18.task1801;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Максимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
