6b243598440e9c7e393cbd06289bb1fca7d1d5be
Aida <abdieva18@mail.ru>
2023-02-03 22:22:21 +0600

    add solution Java Core task 1523

--- 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -5,6 +5,18 @@ package com.javarush.task.task15.task1523;
 */
 
 public class Solution {
+    public Solution(int a){
+
+    }
+    private Solution(int a,int b){
+
+    }
+    protected Solution(int a,int b,int c){
+
+    }
+    Solution(int a,int b,int c,String s){
+
+    }
 
     public static void main(String[] args) {
 
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    public SubSolution(int a) {
+        super(a);
+    }
+
+    protected SubSolution(int a, int b, int c) {
+        super(a, b, c);
+    }
+
+    SubSolution(int a, int b, int c, String s) {
+        super(a, b, c, s);
+    }
+}
