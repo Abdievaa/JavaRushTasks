b51cd0fbd12116b3f9ee9e300b3f74ebe82fd6d6
Aida <abdieva18@mail.ru>
2023-02-21 23:40:04 +0600

    add solution Java Core task 1801

--- 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -10,5 +10,20 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file = reader.readLine();
+        int maxByte = 0;
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            while (inputStream.available() > 0) {
+                int currentByte = inputStream.read();
+                if (currentByte > maxByte){
+                    maxByte = currentByte;
+                }
+            }
+
+        }
+        System.out.println(maxByte);
+
+
     }
 }
