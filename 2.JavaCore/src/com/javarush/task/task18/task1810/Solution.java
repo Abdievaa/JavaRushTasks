95aa18af04b54090e20a7c0b07a5a43ba1148bad
Aida <abdieva18@mail.ru>
2023-02-25 23:48:42 +0600

    add solution Java Core task 1810

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task18.task1810;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+DownloadException
+*/
+
+public class Solution {
+    public static void main(String[] args) throws DownloadException,IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            String files = reader.readLine();
+            try(FileInputStream inputStream = new FileInputStream(files)){
+                if(inputStream.available() < 1000){
+                    throw new DownloadException();
+                }
+            }
+        }
+
+
+    }
+
+    public static class DownloadException extends Exception {
+
+    }
+}
