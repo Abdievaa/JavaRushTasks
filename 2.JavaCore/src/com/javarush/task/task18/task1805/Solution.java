682fc05c7e91061573c6cc47b1a3d2726413976b
Aida <abdieva18@mail.ru>
2023-02-28 18:24:40 +0600

    add solution Java Core task 1805

--- 2.JavaCore/src/com/javarush/task/task18/task1805/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1805/Solution.java
@@ -6,11 +6,27 @@ import java.io.InputStreamReader;
 import java.util.Set;
 import java.util.TreeSet;
 
+
 /* 
 Сортировка байт
 */
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file = reader.readLine();
+        Set<Integer> set = new TreeSet<>();
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            while (inputStream.available() > 0) {
+                int read = inputStream.read();
+                set.add(read);
+
+            }
+//            for (Integer bytes : set) {
+//                System.out.println(bytes + " ");
+//
+//            }
+
+        }
     }
 }
