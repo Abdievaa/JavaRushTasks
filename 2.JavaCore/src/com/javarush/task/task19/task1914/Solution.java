1582bb0b61a1209439ddebb4d1e750f3579f1c3c
Aida <abdieva18@mail.ru>
2023-06-22 22:13:56 +0600

    add solution Java Core task 1914

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1914/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task19.task1914;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Решаем пример
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream printStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(byteArrayOutputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        System.setOut(printStream);
+        byteArrayOutputStream.toString();
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("3 + 6 = ");
+        }
+    }
+}
+
